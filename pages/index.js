ed6096114a9478523e62badf2e9c8a2c3e56fda7
harshitagupta11 <guptaharshita99@gmail.com>
2021-10-02 21:57:28 +0530

    LargeCard and main section completed

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
+import LargeCard from '../components/LargeCard'
 import MediumCard from '../components/MediumCard'
 import SmallCard from '../components/SmallCard'
 
@@ -32,6 +33,15 @@ export default function Home({exploreData,cardsData}) {
           {cardsData?.map(item=>(
             <MediumCard key={item.title} img={item.img} title={item.title}/>
           ))}
+          </div>
+
+          <div>
+            <LargeCard 
+            img='https://links.papareact.com/4cj' 
+            title='The Greatest Outdoors'
+            description='Wishlists curated by Airbnb.'
+            buttonText='Get Inspired'
+            />
           </div>
         </section>
       </main>
